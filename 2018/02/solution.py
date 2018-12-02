719126aabdd4ae9a2c842f84f178db5fb6ee3bfc
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2018-12-02 21:14:21 +0100

    Add day 2 part 2

--- 2018/02/solution.py
+++ 2018/02/solution.py
@@ -20,8 +20,35 @@ def count_twos_and_threes(hashes: list):
     return twos * threes
 
 
+def off_by_one_index(first, second):
+    if first == second:
+        return False
+
+    off_by_index = 0
+    off_by = 0
+    for i in range(0, len(first)):
+        index_of_first = first[i]
+        index_of_second = second[i]
+        if index_of_first != index_of_second:
+            off_by += 1
+            off_by_index = i
+        if off_by > 1:
+            return False
+
+    return off_by_index
+
+
+def generate_secret_hash(hashes: list):
+    for hash in hashes:
+        for hash_cmp in hashes:
+            off_by = off_by_one_index(hash, hash_cmp)
+            if off_by is not False:
+                return hash[:off_by] + hash[off_by+1:]
+
+
 if __name__ == '__main__':
     print('-- Results --')
     with open(r'in.txt') as file:
         hashes = file.readlines()
     print('Result of part 1: {}'.format(count_twos_and_threes(hashes)))
+    print('Result of part 2: {}'.format(generate_secret_hash(hashes)))
--- 2018/02/tests.py
+++ 2018/02/tests.py
@@ -1,15 +1,23 @@
-from solution import count_twos_and_threes
+from solution import count_twos_and_threes, generate_secret_hash
 
 test_twos_and_threes = [
     [['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab'], 12]
 ]
 
 test_off_by_one = [
-    [['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz'], 'fgij']
+    [['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz'], 'fgij'],
+    [['aaaa', 'aaab'], 'aaa'],
+    [['aaaa', 'aabb'], None]
 ]
 
 print('-- TwosThrees Assertions --')
 for hashes, expected in test_twos_and_threes:
     score = count_twos_and_threes(hashes)
     print('{} score={} expected={} [{}]'.format(hashes, score, expected, 'OK' if score == expected else 'NOK'))
+    assert score == expected
+
+print('-- TwosThrees Assertions --')
+for hashes, expected in test_off_by_one:
+    score = generate_secret_hash(hashes)
+    print('{} hash={} expected={} [{}]'.format(hashes, score, expected, 'OK' if score == expected else 'NOK'))
     assert score == expected
